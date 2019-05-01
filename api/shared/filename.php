19310ccba1f26fa9d6d54c8de75fec3587249576
Arjan Vlek <arjan.vlek@42.nl>
2019-05-01 19:33:15 +0200

    Added support for user-submitted update files
    Users will be able to participate by submitting new update file names to
    the database. That way, contributors can more easly obtain update
    download links.

--- api/shared/filename.php
+++ api/shared/filename.php
@@ -19,3 +19,41 @@ function getFilename($downloadUrl) {
 
     return $filename;
 }
+
+function guessOTAVersionFromFilename($filename) {
+    // OP5T full: OnePlus5TOxygen_43_OTA_047_all_1902221932_d08e3bef8111.zip
+    // OP5T incr: OnePlus5TOxygen_43_OTA_047-048_patch_1904191530_e75889ee94bc4.zip
+    // Groups:
+    // 1: OnePlus device (eg 5T). Not present on OPX.
+    // 2: OTA framework version (eg 43)
+    // 3: update revision number (eg 028)
+    // 4: update build date (eg 1902221932)
+    $regex = '/^OnePlus([A-Za-z0-9]*)Oxygen(?:A?)_([0-9]{2})_OTA_(?:[0-9]*-?)([0-9]{3})_(?:all|patch)_([0-9]*)_(?:.*).zip$/';
+    $matches = array();
+
+    preg_match($regex, $filename, $matches);
+    unset($regex);
+
+    if (count($matches) === 5) {
+        $results = array();
+
+        for ($i = 65; $i <= 90; $i++) { // letters 'A' to 'Z'
+            array_push($results,
+                sprintf(
+                    'OnePlus%sOxygen_%d.%s.%d_GLO_%s_%d',
+                    $matches[1],
+                    intval($matches[2]),
+                    chr($i),
+                    intval($matches[3]), // framework version in 2 decimals, e.g. 43).
+                    $matches[3], // framework version in 3 numbers, e.g. 043 instead of 43).
+                    intval($matches[4])
+                )
+            );
+        }
+
+        unset($matches);
+        return $results;
+    } else {
+        return [];
+    }
+}
---
+++ api/v2.3/post_submit_update_file.php
@@ -0,0 +1,82 @@
+<?php
+include '../shared/database.php';
+include '../shared/filename.php';
+
+// Set the return type to JSON.
+header('Content-type: application/json');
+
+// If this class is not accessed over POST or if it is requested from a browser, prevent executing the script.
+if ($_SERVER['REQUEST_METHOD'] !== 'POST' || strpos($_SERVER['HTTP_USER_AGENT'], 'Oxygen_updater_') === FALSE) {
+    http_response_code(403);
+    echo json_encode(array("success" => false, "error_message" => "Access Denied"));
+    die();
+}
+
+// Get the JSON from the request body
+$json = json_decode(file_get_contents('php://input'), true);
+
+$filename = $json['filename'];
+
+// Check if a file name has been set. If not, throw a HTTP 400 Bad Request error.
+if (empty($filename)) {
+    http_response_code(400);
+    echo json_encode(array("success" => false, "error_message" => "Bad Request"));
+    die();
+}
+
+// The filename is part of the Download URL. One would say, that by checking against all stored download URLs, we can see if we already have the submitted file.
+// However, as we only store download URLs of the latest builds, it is impossible to see if an older-submitted update file has already been added to the app.
+// Therefore, check using a possible list of OTA version numbers if the submitted file is already in the app's database.
+// If so, the submission will not be shown to the contributors and administrators of the app.
+$possibleOtaVersionNumbers = guessOTAVersionFromFilename($filename);
+
+$alreadyExistingOtaVersion = null;
+
+$database = connectToDatabase();
+
+if (!empty($possibleOtaVersionNumbers)) {
+    $allVersionNumbers = $database
+        ->query('SELECT DISTINCT(parent_version_number) FROM update_data UNION SELECT DISTINCT(ota_version_number) FROM update_data')
+        ->fetchAll(PDO::FETCH_COLUMN);
+
+    foreach ($possibleOtaVersionNumbers as $possibleOtaVersionNumber) {
+        if (in_array($possibleOtaVersionNumber, $allVersionNumbers)) {
+            $alreadyExistingOtaVersion = $possibleOtaVersionNumber;
+            break;
+        }
+    }
+
+    unset($allVersionNumbers);
+}
+
+unset($possibleOtaVersionNumbers);
+
+// Check if this file name has been submitted before
+$query = $database->prepare("SELECT COUNT(*) as count FROM submitted_update_file where `name` = :filename");
+$query->bindParam(':filename', $filename);
+$query->execute();
+$timesSubmittedBefore = $query->fetch(PDO::FETCH_ASSOC)['count'];
+unset($query);
+if ($timesSubmittedBefore == 0) {
+    // If it has never been submitted before, create a new entry for it.
+    $query = $database->prepare("INSERT INTO submitted_update_file(`name`, ota_version_number, times_submitted) VALUES (:filename, :ota_version_number, 1)");
+
+} else {
+    // Else, increment the submit count of the existing entry.
+    $query = $database->prepare("UPDATE submitted_update_file SET times_submitted = times_submitted + 1, ota_version_number = :ota_version_number where `name` = :filename");
+}
+
+$query->bindParam(':filename', $filename);
+$query->bindParam(':ota_version_number', $alreadyExistingOtaVersion); // null when not exists
+$query->execute();
+
+unset($json);
+unset($filename);
+unset($previouslySubmittedUpdateFileQuery);
+unset($alreadyExistingOtaVersion);
+unset($database);
+unset($timesSubmittedBefore);
+
+echo json_encode(array("success" => $query->rowCount() > 0, "error_message" => $query->rowCount() === 0 ? 'Error storing submitted update file' : null));
+
+unset($query);
