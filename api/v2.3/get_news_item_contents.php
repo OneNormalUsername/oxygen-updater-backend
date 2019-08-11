a5222652246e0c07d423f454b8d53602d8bb222c
Arjan Vlek <arjan1995@gmail.com>
2019-08-11 16:21:56 +0200

    Improved news contents HTML response (useful for direct links to API):
    - Responsive, so direct calls to the API now look good on mobile
    - Returns the title and description of the news story in the HTML
    - Valid HTML5 doctype
    - Input validation on query parameters to prevent returning malformed
    HTML

--- api/v2.3/get_news_item_contents.php
+++ api/v2.3/get_news_item_contents.php
@@ -6,14 +6,24 @@ $database = connectToDatabase();
 
 // Obtain all required request parameters.
 $newsItemId = $_GET["news_item_id"];
-$language = $_GET["language"];
+// Escape the language parameter as its directly written to the HTML
+$language = substr(strtolower($_GET["language"]), 0, 2);
 $theme = $_GET["theme"]; // Light, Dark or "" ("" only on app 2.7.6 and older)
 
+// Input validation
+if ($language !== 'nl' && $language !== 'en') {
+    die();
+}
+
+if ($theme !== 'Light' && $theme !== 'Dark' && $theme !== '') {
+    die();
+}
+
 // Execute the query
-if (strtolower($language) === 'nl') {
-  $query = $database->prepare("SELECT dutch_text as content FROM news_item WHERE id = :id AND published = TRUE");
+if ($language === 'nl') {
+    $query = $database->prepare("SELECT dutch_text as content, dutch_title as title, dutch_subtitle as description FROM news_item WHERE id = :id AND published = TRUE");
 } else {
-  $query = $database->prepare("SELECT english_text as content FROM news_item WHERE id = :id AND published = TRUE");
+    $query = $database->prepare("SELECT english_text as content, english_title as title, english_subtitle as description FROM news_item WHERE id = :id AND published = TRUE");
 }
 
 $query->bindParam(":id", $newsItemId);
@@ -22,7 +32,10 @@ $query->execute();
 // Return the output as HTML
 header('Content-type: text/html');
 $style = "";
-$contents = $query->fetch(PDO::FETCH_ASSOC)['content'];
+$result = $query->fetch(PDO::FETCH_ASSOC);
+$title = $result['title'];
+$description = $result['description'];
+$contents = $result['content'];
 
 if ($theme === 'Dark') {
     $style = "<style>
@@ -45,9 +58,13 @@ if ($theme === 'Dark') {
 }
 
 echo "
-        <html lang=" . $language . ">
+        <!DOCTYPE html>
+        <html lang=\"" . $language . "\" dir=\"ltr\">
           <head>
             ". $style . "
+            <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+            <meta name='Description' content='". $description . "'>
+            <title>" . $title . "</title>
           </head>
           <body>
           " . $contents . "
